a87154cf4acbe3d4b182d94d8df3b2a266f2b5a7
baldo-vela <baldo.vela+github@gmail.com>
2021-01-10 19:43:19 -0600

    expansion of vacccine model for sorting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    has_many :vaccines
+    
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
--- app/models/vaccine.rb
+++ app/models/vaccine.rb
@@ -1,2 +1,22 @@
 class Vaccine < ActiveRecord::Base
+    has_many :users
+
+    validates_presence_of :lot_no, :mfr, :expir
+    validates_uniqueness_of :lot_no
+
+    def self.search(query)
+        where("title LIKE ?", "%#{query}%")
+    end
+
+    def self.sort
+        order(lot_no: :asc)
+    end
+
+    def self.by_mfr
+        where(mfr: mfr)
+    end
+
+    def self.all_mfr
+        self.pluck(:mfr).uniq
+    end 
 end
