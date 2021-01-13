7f323201e51b338d89d4cd38a1150f3ef136a9a8
baldo-vela <baldo.vela+github@gmail.com>
2021-01-12 21:20:00 -0600

    Adding more features to Vaccine Controller w/User

--- app/controllers/vaccines_controller.rb
+++ app/controllers/vaccines_controller.rb
@@ -1,38 +1,90 @@
-class VaccinesController < ApplicationController
-
-  # GET: /vaccines
-  get "/vaccines" do
-    @vaccines = Vaccine.all
-    erb :"/vaccines/index.html"
+class VaccineController < ApplicationController
+  get '/vaccines' do
+    redirect_if_not_logged_in
+    if params[:query] # only exist if search is filled out
+      # search for vaccines based on query
+      @vaccines = current_user.vaccines.search(params[:query])
+    elsif params[:filter]
+      # @vaccines = list of vaccines that are filtered by genre
+      @vaccines = current_user.vaccines.by_genre(params[:filter]).alphabetize
+    else # search is not filled out so show all vaccines
+      @vaccines = current_user.vaccines.alphabetize
+    end
+    @vaccine = vaccine.find_by_id(session[:vaccine_id])
+    erb :'vaccines/index'
   end
 
-  # GET: /vaccines/new
-  get "/vaccines/new" do
-    erb :"/vaccines/new.html"
+  get '/vaccines/new' do
+    redirect_if_not_logged_in
+    erb :'vaccines/new'
+  end
+  
+  get '/vaccines/:id' do
+    redirect_if_not_logged_in
+    find_vaccine
+    redirect_if_not_owner
+    session[:vaccine_id] = @vaccine.id if @vaccine
+    redirect_if_vaccine_not_found
+    erb :'vaccines/show'
+  end
+  
+  get '/vaccines/:id/edit' do
+    redirect_if_not_logged_in
+    find_vaccine
+    redirect_if_vaccine_not_found
+    redirect_if_not_owner
+    erb :'vaccines/edit'
+  end
+  
+  post '/vaccines' do
+    vaccine = vaccine.new(params[:vaccine])
+    if vaccine.save
+      current_user.vaccines << vaccine
+      redirect '/vaccines'
+    else
+      flash[:errors] = vaccine.errors.full_messages
+      redirect '/vaccines/new'
+    end
   end
 
-  # POST: /vaccines
-  post "/vaccines" do
-    redirect "/vaccines"
+  post '/vaccines/add' do
+    @vaccine = vaccine.find_by_id(params[:vaccine][:id])
+    if @vaccine && !current_user.vaccines.include?(@vaccine)
+      current_user.vaccines << @vaccine
+      redirect "/vaccines"
+    else
+      redirect "/vaccines/new"
+    end
   end
-
-  # GET: /vaccines/5
-  get "/vaccines/:id" do
-    erb :"/vaccines/show.html"
-  end
-
-  # GET: /vaccines/5/edit
-  get "/vaccines/:id/edit" do
-    erb :"/vaccines/edit.html"
-  end
-
-  # PATCH: /vaccines/5
+  
   patch "/vaccines/:id" do
-    redirect "/vaccines/:id"
+    find_vaccine
+    redirect_if_vaccine_not_found
+    if @vaccine.update(params[:vaccine])
+      redirect "/vaccines/#{@vaccine.id}"
+    else
+      redirect "/vaccines/#{@vaccine.id}/edit"
+    end
+  end
+  
+  delete '/vaccines/:id' do
+    find_vaccine
+    redirect_if_vaccine_not_found
+    redirect_if_not_owner
+    @vaccine.destroy
+    redirect "/vaccines"
+  end
+  
+  private
+  def find_vaccine
+    @vaccine = vaccine.find_by_id(params[:id])
+  end
+  
+  def redirect_if_vaccine_not_found
+    redirect "/vaccines" unless @vaccine
   end
 
-  # DELETE: /vaccines/5/delete
-  delete "/vaccines/:id/delete" do
-    redirect "/vaccines"
+  def redirect_if_not_owner
+    redirect "/vaccines" unless @vaccine.user == current_user
   end
 end
